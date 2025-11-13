69434d7c3076e3ee761e11cca71d85bc42360a23
Anodeluxe <azfanovasammyrafifsaputra@mail.ugm.ac.id>
2025-11-13 23:33:23 +0700

    feat: more responsive Landing Page Hero

--- frontend/src/components/LandingPage/LandingHero.jsx
+++ frontend/src/components/LandingPage/LandingHero.jsx
@@ -13,32 +13,37 @@ export const LandingHero = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4 lg:gap-5 h-full">
           {/* Sajak Hari Ini */}
           <div className="h-full flex flex-col">
-            <a href="" className="group relative flex flex-col items-center grow overflow-hidden rounded-lg md:rounded-2xl border bg-gray-950 text-background">
+            <a href="" className="group relative flex flex-col items-center grow overflow-hidden rounded-lg md:rounded-2xl border bg-gray-950 text-background pt-2 md:pt-4 pb-12 px-4">
               <div className="absolute inset-0"></div>
-              <h1 className="text-8xl mb-8"><span className="font-instrument">SAJAK</span><span className="font-bold"> HARI INI</span></h1>
-              <h2 className="font-instrument italic text-5xl">judul</h2>
-              <h3 className="text-2xl font-extralight mb-8">nama penulis</h3>
-              <p className="font-instrument text-3xl">placeholder placeholder placeholder</p>
-              <p className="font-instrument text-3xl">placeholder placeholder placeholder</p>
-              <p className="font-instrument text-3xl">placeholder placeholder placeholder</p>
-              <a className="absolute bg-cerise bottom-0 right-0 mr-6 mb-6 px-6 py-2.5 rounded-2xl text-xl font-bold">Baca Selengkapnya ...</a>
+              <h1 className="text-4xl md:text-4xl lg:text-6xl 2xl:text-8xl mb-6 md:mb-8"><span className="font-instrument">SAJAK</span><span className="font-bold"> HARI INI</span></h1>
+              <h2 className="font-instrument italic text-2xl md:text-3xl xl:text-5xl">judul</h2>
+              <h3 className="text-lg xl:text-2xl font-extralight mb-4 md:mb-8">nama penulis</h3>
+              <p className="font-instrument text-md xl:text-3xl">placeholder placeholder placeholder</p>
+              <h4 className="absolute bg-cerise bottom-0 right-0 mr-4 mb-4 xl:mr-6 xl:mb-6 px-4 md:px-6 py-2.5 rounded-lg md:rounded-2xl text-sm xl:text-xl font-bold">Baca Selengkapnya ...</h4>
             </a>
           </div>
           {/* ---- kolom kanan ---- */}
           <div className="h-full flex flex-col gap-2 md:gap-4 lg:gap-5">
             {/* Tulis Sajak */}
-            <a href="" className="group relative flex md:grow overflow-hidden rounded-lg md:rounded-2xl h-20 md:h-auto border">
+            <a href="" className="group relative flex md:grow overflow-hidden rounded-lg md:rounded-2xl h-full md:h-auto border">
               <Image
                   src={Proklamasi}
-                  className="absolute"
-                  layout="fit" 
-                  objectFit="contain"
+                  className="hidden md:block absolute xl:-translate-y-10 "
+                  fill="none"
                   alt="ilustrasi tangan menulis"
                 />
-              <div className="absolute flex flex-col-reverse md:flex-row md:items-center justify-between w-full h-full md:h-auto bottom-0 mb-2.5">
-                <h1 className="font-jakarta font-bold text-xl md:text-4xl xl:text-6xl 2xl:text-[5.3rem] text-nowrap ml-2.5">TULIS SAJAK</h1>
-                <WritePlus className="h-[30px] md:h-12 2xl:h-[72px] w-auto mr-3" />
+              <Image
+                  src={Proklamasi}
+                  className="md:hidden absolute w-full h-[150%] -translate-x-[25%] -translate-y-[30%]"
+                  height={5000}
+                  alt="ilustrasi tangan menulis"
+                />
+              <div className="hidden absolute md:flex items-center justify-between w-full h-auto bottom-0 right-0 mb-2.5">
+                <h1 className="font-jakarta font-bold text-4xl xl:text-6xl 2xl:text-[5.3rem] text-nowrap ml-2.5">TULIS SAJAK</h1>
+                <WritePlus className="md:h-10 2xl:h-[72px] mr-3" />
               </div>
+              <h1 className="md:hidden absolute bottom-0 right-0 font-jakarta font-bold text-2xl text-nowrap">TULIS SAJAK</h1>
+              <WritePlus className="md:hidden absolute right-0 top-0 h-[30px] md:h-12 2xl:h-[72px] w-auto" />
             </a>
             {/* bantuan */}
             <a href="" className="group relative flex flex-row items-center justify-between overflow-hidden rounded-lg md:rounded-2xl h-fit py-2.5 md:py-4 px-2.5 md:px-6 border">
