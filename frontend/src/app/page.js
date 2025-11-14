871519cb94289559d1a14bd8b4a89925d0eec99a
Anodeluxe <azfanovasammyrafifsaputra@mail.ugm.ac.id>
2025-11-14 21:45:33 +0700

    feat: Agenda Komunitas (landing page)

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { LandingHero } from "@/components/LandingPage/LandingHero";
 import { LandingTerbaru } from "@/components/LandingPage/LandingTerbaru";
+import { AgendaKomunitas } from "@/components/LandingPage/AgendaKomunitas";
+import { ReviewPenulis } from "@/components/LandingPage/ReviewPenulis";
 
 export default function Home() {
   return (
     <div>
       <LandingHero />
       <LandingTerbaru />
-      <div className="pb-30">aaaaaaaa</div>
+      <div className="pb-30 bg-hitam">aaaaaaaa</div>
+      <AgendaKomunitas />
+      <ReviewPenulis />
+      <div className="pb-30 bg-putih"> </div>
     </div>
   )
 }
