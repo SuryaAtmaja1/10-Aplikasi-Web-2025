bfa75ad8054364eb6ffd0c00f07068f1276d07f6
Anodeluxe <azfanovasammyrafifsaputra@mail.ugm.ac.id>
2025-11-16 21:24:52 +0700

    feat: Lokal category Hero

--- frontend/src/components/CategoryPage/heroes/LokalHero.jsx
+++ frontend/src/components/CategoryPage/heroes/LokalHero.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Image from "next/image";
+import LokalHeroImage from "../../../../public/assets/category-sajak/lokal-hero.png"
 
 export const LokalHero = () => {
-  return <div>LokalHero</div>;
+  return (
+  <div className="relative flex">
+    <Image
+      src={LokalHeroImage}
+      className="w-full"
+      width={1920}
+      height={1280}
+      alt="LokalHeroImage"
+    />
+  </div>
+  );
 };
