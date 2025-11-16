6af95be110ac723506b6c9b9f9b68b1e5e09ff1d
Anodeluxe <azfanovasammyrafifsaputra@mail.ugm.ac.id>
2025-11-16 21:52:13 +0700

    feat: Sosial category Hero & Ekonomi category Hero

--- frontend/src/components/CategoryPage/heroes/SosialHero.jsx
+++ frontend/src/components/CategoryPage/heroes/SosialHero.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import Image from "next/image";
+import SosialHeroImg from "../../../../public/assets/category-sajak/sosial-hero.svg"
+import SosialHeroImgMobile from "../../../../public/assets/category-sajak/sosial-hero-mobile.svg"
 
 export const SosialHero = () => {
-  return <div>SosialHero</div>;
+  return (
+  <div className="relative flex w-screen overflow-hidden border-b">
+    {/* Desktop */}
+    <div className="hidden md:block">
+      <Image
+        src={SosialHeroImg}
+        className="w-full"
+        width={1920}
+        height={800}
+        alt="SosialHeroImg"
+      />
+      <div className="h-[100px] px-10">
+        <h2 className="place-self-end font-instrument text-3xl">xxxx sajak, xx penulis</h2>
+      </div>
+    </div>
+
+    {/* Mobile */}
+    <div className="md:hidden w-full">
+      <Image
+        src={SosialHeroImgMobile}
+        className="w-full"
+        width={360}
+        height={240}
+        alt="SosialHeroImgMobile"
+      />
+      <div className="h-[100px] px-2">
+        <h2 className="place-self-end font-instrument text-xl">xxxx sajak, xx penulis</h2>
+      </div>
+    </div>
+  </div>
+  );
 };
